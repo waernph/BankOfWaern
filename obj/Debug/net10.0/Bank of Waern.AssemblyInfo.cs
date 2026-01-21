4fd95f1f85ab646e5b1336479b30afdadb13b6c8
Philip Waern <waernph@gmail.com>
2026-01-21 19:45:30 +0100

    Removed textfile with function list

--- obj/Debug/net10.0/Bank of Waern.AssemblyInfo.cs
+++ obj/Debug/net10.0/Bank of Waern.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank of Waern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c38594dc46a8360650d5b1be12e14c519cd2133e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e907491e10039ce5ec2b8533a8c9f87484f44fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank of Waern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank of Waern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
