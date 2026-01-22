cdd2f4357515300dd5fd2f15ea16b6146a2fa3ce
Philip Waern <waernph@gmail.com>
2026-01-22 07:07:27 +0100

    Edited Readme

--- obj/Debug/net10.0/Bank of Waern.AssemblyInfo.cs
+++ obj/Debug/net10.0/Bank of Waern.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank of Waern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e907491e10039ce5ec2b8533a8c9f87484f44fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2d7a9a49a45b0afbeca4adf298f489d8b231ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank of Waern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank of Waern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
